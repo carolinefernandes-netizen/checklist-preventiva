d1d2bf98604dd7a110a8ee6b282809facdb4a0b6
carolinefernandes-netizen <caroline.fernandes@belcito.com.br>
2026-06-29 16:28:40 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'belcito-v1';
+const ASSETS = [
+  '/checklist-preventiva/',
+  '/checklist-preventiva/index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Só faz cache de recursos locais; Firebase/CDN vai direto pra rede
+  if (!e.request.url.includes('checklist-preventiva')) return;
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
